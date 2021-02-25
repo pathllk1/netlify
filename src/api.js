678c2ec4a2a2cfccf2798dbc88802143ce60339f
anjanvkp <anjanvkp@gmail.com>
2021-02-25 18:21:34 +0530

    5

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const path = require('path');
 
 const app = express();
 const router = express.Router();
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
+router.get('/', (req, res) => {
+    res.render("index");
+})
 
 app.use("/.netlify/functions/api", router);
 
