397edd28b54e899d5b499630bfef6df807f954cf
anjanvkp <anjanvkp@gmail.com>
2021-02-26 10:36:07 +0530

    7

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const router = express.Router();
-app.set('view engine', 'ejs');
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/', (req, res) => {
